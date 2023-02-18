7d34773f66019e042ea085077f03c5667c0012d1
kinoshita kazuoki <kino05040826@gmail.com>
2023-02-18 17:04:20 +0900

    アクションメソッドの共通化

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -1,4 +1,6 @@
 class PicturesController < ApplicationController
+  before_action :set_picture, only: [:show, :edit, :update, :destroy]
+
   def index
     @pictures = Picture.all
   end
@@ -17,15 +19,12 @@ class PicturesController < ApplicationController
   end
 
   def show
-    @picture = Picture.find(params[:id])
   end
   
   def edit
-    @picture = Picture.find(params[:id])
   end
 
   def update
-    @picture = Picture.find(params[:id])
     if @picture.update(picture_params)
       redirect_to pictures_path, notice: "投稿を編集しました！"
     else
@@ -34,13 +33,17 @@ class PicturesController < ApplicationController
   end
 
   def destroy
-    @picture = Picture.find(params[:id])
     @picture.destroy
     redirect_to pictures_path, notice: "投稿を削除しました！"
   end
   
   private
+  
   def picture_params
     params.require(:picture).permit(:content, :image)
+  end
+
+  def set_picture
+    @picture = Picture.find(params[:id])
   end
 end
