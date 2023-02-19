9ef78082759693a9702afa24300baea05ce6eaba
kinoshita kazuoki <kino05040826@gmail.com>
2023-02-19 12:37:00 +0900

    user編集機能実装

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,7 +6,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      redirect_to user_path(@user.id)
+      redirect_to user_path(@user.id), notice: "アカウントを作成しました！"
     else
       render :new
     end
@@ -16,9 +16,23 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
   end
 
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      redirect_to edit_user_path, notice: "プロフィールを編集しました！"
+    else
+      render :edit
+    end
+  end
+
+
   private
   
   def user_params
-    params.require(:user).permit(:email, :full_name, :user_name, :password, :password_confirmation)
+    params.require(:user).permit(:email, :full_name, :user_name, :password, :password_confirmation, :image)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,5 @@ Rails.application.routes.draw do
     end
   end
   
-  resources :users, only: [:new, :create, :show]
+  resources :users, only: [:new, :create, :show, :edit, :update]
 end
