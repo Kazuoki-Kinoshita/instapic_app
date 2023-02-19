d86766a43b5d0c59bbcd255496a34f045f676abd
kinoshita kazuoki <kino05040826@gmail.com>
2023-02-19 20:37:05 +0900

    お気に入りのpicture一覧機能実装

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,11 +1,15 @@
 class FavoritesController < ApplicationController
   def create
     favorite = current_user.favorites.create(picture_id: params[:picture_id])
-    redirect_to pictures_path, notice: "#{favorite.picture.user.user_name}さんのブログをお気に入りに登録しました！"
+    redirect_to favorites_path, notice: "#{favorite.picture.user.user_name}さんのブログをお気に入りに登録しました！"
   end
-  
+
+  def index
+    @favorites = current_user.favorites
+  end
+
   def destroy
     favorite = current_user.favorites.find_by(id: params[:id]).destroy
-    redirect_to pictures_path, notice: "#{favorite.picture.user.user_name}さんのブログのお気に入りを解除しました！"
+    redirect_to favorites_path, notice: "#{favorite.picture.user.user_name}さんのブログのお気に入りを解除しました！"
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
   resources :pictures do
     collection do
-      post :confirm
+      post :confirm 
     end
   end
   resources :users, only: [:new, :create, :show, :edit, :update]
   resources :sessions, only: [:new, :create, :destroy]
-  resources :favorites, only: [:create, :destroy]      
+  resources :favorites, only: [:index, :create, :destroy]      
 end
