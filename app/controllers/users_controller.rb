af4c6e043d719144dfed82caaee70fa4645fc366
kinoshita kazuoki <kino05040826@gmail.com>
2023-02-19 10:24:10 +0900

    user登録失敗時の機能実装

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,6 +4,18 @@ class UsersController < ApplicationController
   end
 
   def create
-    
+    @user = User.new(user_params)
+    if @user.save
+      
+    else
+      render :new
+    end
+
+  end
+
+  private
+  
+  def user_params
+    params.require(:user).permit(:email, :full_name, :user_name, :password, :password_confirmation)
   end
 end
