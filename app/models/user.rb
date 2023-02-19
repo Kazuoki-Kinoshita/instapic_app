99fbaf0872c84b1804370001c59496509a7025c2
kinoshita kazuoki <kino05040826@gmail.com>
2023-02-19 20:41:30 +0900

    プロフィール編集時のバリデーションを設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,7 @@ class User < ApplicationRecord
              uniqueness: true
   before_validation { email.downcase! }
   has_secure_password
-  validates :password, length: { minimum: 6}
+  validates :password, length: { minimum: 6}, on: :create
   has_many :pictures
   has_many :favorites, dependent: :destroy
   has_many :favorite_pictures, through: :favorites, source: :picture
